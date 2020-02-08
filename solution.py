01ec6acc85088e9f0151fee2ef5260476300e6b0
JMarcan <jan.marcan@protonmail.com>
2020-02-08 16:58:07 +0100

    feat: implement depth first search and constraint propagation

--- solution.py
+++ solution.py
@@ -14,8 +14,7 @@ unitlist = unitlist
 # Must be called after all units (including diagonals) are added to the unitlist
 units = extract_units(unitlist, boxes)
 peers = extract_peers(units, boxes)
-
-
+    
 def naked_twins(values):
     """Eliminate values using the naked twins strategy.
 
@@ -54,6 +53,8 @@ def naked_twins(values):
     https://github.com/udacity/artificial-intelligence/blob/master/Projects/1_Sudoku/pseudocode.md
     """
     # TODO: Implement this function!
+    
+    
     raise NotImplementedError
 
 
@@ -73,8 +74,18 @@ def eliminate(values):
     dict
         The values dictionary with the assigned values eliminated from peers
     """
+    
+    
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit,'')
+    return values
+    
+    
     # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    #raise NotImplementedError
 
 
 def only_choice(values):
@@ -92,13 +103,13 @@ def only_choice(values):
     -------
     dict
         The values dictionary with all single-valued boxes assigned
-
-    Notes
-    -----
-    You should be able to complete this function by copying your code from the classroom
     """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                values[dplaces[0]] = digit
+    return values
 
 
 def reduce_puzzle(values):
@@ -115,8 +126,23 @@ def reduce_puzzle(values):
         The values dictionary after continued application of the constraint strategies
         no longer produces any changes, or False if the puzzle is unsolvable 
     """
-    # TODO: Copy your code from the classroom and modify it to complete this function
-    raise NotImplementedError
+    
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        # Use the Eliminate Strategy
+        values = eliminate(values)
+        # Use the Only Choice Strategy
+        values = only_choice(values)
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 
 def search(values):
@@ -132,14 +158,25 @@ def search(values):
     -------
     dict or False
         The values dictionary with all boxes assigned or False
-
-    Notes
-    -----
-    You should be able to complete this function by copying your code from the classroom
-    and extending it to call the naked twins strategy.
     """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    
+    values = reduce_puzzle(values)
+    
+    if values is False:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes): 
+        return values ## Solved!
+    # Choose one of the unfilled squares with the fewest possibilities
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    # Now use recurrence to solve each one of the resulting sudokus, and 
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
+        
+    return new_sudoku
 
 
 def solve(grid):
@@ -158,16 +195,25 @@ def solve(grid):
         The dictionary representation of the final sudoku grid or False if no solution exists.
     """
     values = grid2values(grid)
+    
     values = search(values)
+
+    print ("Grid after search:")
+    display(values)
+    
     return values
 
 
 if __name__ == "__main__":
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+    print ("Original grid:")
     display(grid2values(diag_sudoku_grid))
     result = solve(diag_sudoku_grid)
+    print ("Result grid:")
     display(result)
 
+
+  
     try:
         import PySudoku
         PySudoku.play(grid2values(diag_sudoku_grid), result, history)
